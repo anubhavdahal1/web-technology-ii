5373a623a25ac932edfe0bf5a21142318be666f3
anubhavdahal <dahalanubhav83@gmail.com>
2026-06-11 12:30:03 +0545

    first commit

---
+++ basics.php
@@ -0,0 +1,54 @@
+<?php
+
+// datatypes in php
+
+// $var="Hello, World!";
+// $var=123;
+// $var=true;
+// $var=null;
+// $var=123.123;
+// $var=array(1,2,3,4,5,'a');
+// $var=[1,2,3,4,5,'a'];
+// echo $var;
+// var_dump($var);
+// print_r($var);  
+
+
+
+class Person{
+
+}
+$obj=new Person();
+// var_dump($obj);
+
+
+//operators in php
+
+$a=22;
+$b=20;  
+// $c=$a+$b;
+// echo $c;
+// echo $a-$b;
+// echo $a*$b;
+// echo $a/$b;
+//echo $a%$b;  //modulus operator
+
+// echo $a++;
+// echo "<br>";
+// echo $a++;
+// echo $a;
+
+// echo ++$a;
+
+// echo $a--;
+// echo "<br>";        
+// echo $a;
+//  echo --$a;
+
+$c=10;
+//$c+=5;  // $c=$c+5
+//echo $c;
+
+$c-=5;  // $c=$c-5
+echo $c;
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// echo"hello world!!";
+// print_r("here is common measure text");
+// var_dump("this is a string");
+?>
