d244a65c8a214aa32fc1cadd94b166e156775385
anubhavdahal <dahalanubhav83@gmail.com>
2026-06-12 16:41:23 +0545

    first commit

--- basics.php
+++ basics.php
@@ -12,18 +12,15 @@
 // echo $var;
 // var_dump($var);
 // print_r($var);  
-
-
-
 class Person{
-
+    
 }
 $obj=new Person();
 // var_dump($obj);
 
 
 //operators in php
-
+// 1. Arithmetic operators
 $a=22;
 $b=20;  
 // $c=$a+$b;
@@ -44,11 +41,65 @@ $b=20;
 // echo "<br>";        
 // echo $a;
 //  echo --$a;
-
+// 2. Assignment operators
 $c=10;
 //$c+=5;  // $c=$c+5
 //echo $c;
 
 $c-=5;  // $c=$c-5
-echo $c;
+// echo $c;
+// +=,-=,*=,/=,%=,**=
+$s=2;
+$s**=3;  // $s=$s**3
+// echo $s;
+
+// 3. Comparison operators (>,>=,<,<=,!=,==,==)
+// $p=3;
+// $q=2;
+// $x=$p>$q;
+// echo $x;
+
+/*4. logical operators(&&,||,!)
+ $isAllowed=false;
+ if(!$isAllowed){
+    echo "You are good to go";
+ }
+ */
+/*5. string operators(., .=)
+$fname="Ram";
+$lname="Yadav";
+$fullname=$fname.$lname;
+// echo $fullname;
+$name="Hari";
+$name .="Karki";
+ echo $name;
+ */
+/*6  ternary operator
+(condition)?"true statement":"false statement" */
+$d=1;
+$e=4;
+echo ($d>$e)? "D is greater than e":"d is not greater than e";
+
+
+
+/*control structure 
+1. conditional statements
+2. loop statements
+
+1. conditional statements
+
+if
+*/
+$a=4;
+// if($a>5){
+//     echo "The given number is greater than 5";
+// }
+
+// if else 
+// if($a>5){
+//     echo "The given number is greater than 5";
+// }
+// else{
+//     echo "The given number is less than or equal to 5";
+// }
 ?>
