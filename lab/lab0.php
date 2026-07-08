447f09a307c8cc8745c4a909a3584017dd54fbbe
anubhavdahal <dahalanubhav83@gmail.com>
2026-07-08 16:00:45 +0545

    lab work

---
+++ lab/lab0.php
@@ -0,0 +1,46 @@
+<?php
+// echo "I am IT student";
+
+
+/* $age=24;
+if($age<=12){
+    echo "You are a child";
+}
+else if($age>12 &&$age<=19){
+    echo "You are teenager";
+}
+else if($age>19 &&$age<=40){
+    echo "You are as adult as Mr Dan Bahadur Adhikari";
+}
+else{
+    echo "You are as old as Ronaldo and you might recall spain ";
+} */
+
+
+/*
+   for($i=0;$i<=10;$i++){
+    // echo $i;
+    // echo "<br>";
+
+    for($j=0;$j<=$i;$j++){
+        echo $j;
+    }
+    echo "<br>";
+   } 
+    */
+
+/*    model question Qn:17 
+syntax:
+function functionname(arguments){
+body of a function
+} 
+Example: Function to add Two numbers and display the result;
+*/
+function add(){
+    $a=5;
+    $b=3;
+    $result=$a+$b;
+    echo "The sum of $a and $b = $result";
+}
+add();
+?>
--- lab/lab1.php
+++ lab/lab1.php
@@ -1,13 +1,19 @@
 <?php
-$raw_products = ["    t-shirt:25.99:5 ", "JEANS:49.95:0", " jacket : 89.00 : 2 "];
+$raw_products = ["    t-shirt:25.99:5    ", "JEANS:49.95:0", "    jacket : 89.00 : 2 "];
 // print_r($raw_products);
 $newproductsAssoc=[];
 foreach($raw_products as $product) {
-//   print_r($product);
-//   echo "<br>";
+  // var_dump($product);
+  // echo "<br>";
 $whiteSpaceRemovedProduct = trim($product);
+  // var_dump($whiteSpaceRemovedProduct);
+  // echo "<br>";
 $lowercase=strtolower($whiteSpaceRemovedProduct);
+// print_r($lowercase);
+// echo "<br>";
 $newProductArray= explode(":", $lowercase);
+// print_r($newProductArray);
+// echo "<br>";
 $newproductsAssoc[] = [
     "name" => $newProductArray[0],
     "price" => $newProductArray[1],
@@ -15,4 +21,5 @@ $newproductsAssoc[] = [
   ];
 }
   print_r($newproductsAssoc);
+
 ?>
