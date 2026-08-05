5667fb762ef362f0a24869167c7b6556206511dd
anubhavdahal <dahalanubhav83@gmail.com>
2026-08-05 12:57:22 +0545

    abstract

---
+++ opps/abstract.php
@@ -0,0 +1,43 @@
+<?php
+// ## Abstract Class
+syntax:
+abstract class ClassName
+{
+    abstract public function methodName();
+
+    public function normalMethod()
+    {
+        // Code
+    }
+}
+
+// Example: 
+
+abstract class Shape
+{
+    abstract public function area();
+
+    public function display()
+    {
+        echo "Calculating Area...<br>";
+    }
+}
+
+class Rectangle extends Shape
+{
+    private $length = 10;
+    private $breadth = 5;
+
+    public function area()
+    {
+        echo "Area = " . ($this->length * $this->breadth);
+    }
+}
+
+$obj = new Rectangle();
+
+$obj->display();
+$obj->area();
+
+
+?>
